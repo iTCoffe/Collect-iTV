4225dccde84761233d917178aa35ee530cfe6e80
IT咖啡館 <176115520+iTCoffe@users.noreply.github.com>
2025-08-30 17:23:22 +0800

    Add files via upload

--- .github/workflows/iptv_keep.py
+++ .github/workflows/iptv_keep.py
@@ -322,13 +322,13 @@ def generate_output_files(valid_urls, cctv_channels, province_channels, m3u_file
 
         # 添加新的 EXTINF 行
         f.write("#EXTINF:-1 tvg-id=\"温馨提示\" tvg-name=\"温馨提示\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/温馨提示.png\" group-title=\"🦧温馨提示\",温馨提示\n")
-        f.write("https://itcoffe.5iclub.us.kg/Loong/iTV/Disclaimer.mp4\n")
+        f.write("https://loong.5iclub.ctrl.li/video/Disclaimer.mp4\n")
 
         f.write("#EXTINF:-1 tvg-id=\"谨防诈骗\" tvg-name=\"谨防诈骗\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/谨防诈骗.png\" group-title=\"🦧温馨提示\",谨防诈骗\n")
-        f.write("https://itcoffe.5iclub.us.kg/Loong/iTV/Disclaimer.mp4\n")
+        f.write("https://loong.5iclub.ctrl.li/video/Disclaimer.mp4\n")
 
         f.write("#EXTINF:-1 tvg-id=\"禁止蕉绿\" tvg-name=\"禁止蕉绿\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/禁止蕉绿.png\" group-title=\"🦧温馨提示\",禁止蕉绿\n")
-        f.write("https://itcoffe.5iclub.us.kg/Loong/iTV/Disclaimer.mp4\n")
+        f.write("https://loong.5iclub.ctrl.li/video/Disclaimer.mp4\n")
         
         # 写入频道信息
         for channel_info in deduped_channels:
