1efe1160f2405082b5605b21a6bd914fc4692393
IT咖啡館 <176115520+iTCoffe@users.noreply.github.com>
2025-11-20 21:42:16 +0800

    Update iptv.py

--- .github/workflows/iptv.py
+++ .github/workflows/iptv.py
@@ -318,17 +318,17 @@ def generate_output_files(valid_urls, cctv_channels, province_channels, m3u_file
     # 写入 M3U 文件
     with open(m3u_filename, 'w', encoding='utf-8') as f:
         # 添加带有所需属性的标题行
-        f.write("#EXTM3U x-tvg-url=\"https://itv.5iclub.dpdns.org/epg.xml\" catchup=\"append\" catchup-source=\"?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}\"\n")
+        f.write("#EXTM3U x-tvg-url=\"https://itv.sspai.pp.uag/epg.xml\" catchup=\"append\" catchup-source=\"?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}\"\n")
 
         # 添加新的 EXTINF 行
-        f.write("#EXTINF:-1 tvg-id=\"温馨提示\" tvg-name=\"温馨提示\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/温馨提示.png\" group-title=\"🦧温馨提示\",温馨提示\n")
-        f.write("https://iCloud.5iclub.dpdns.org/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"温馨提示\" tvg-name=\"温馨提示\" tvg-logo=\"https://logo.sspai.pp.ua/img/温馨提示.png\" group-title=\"🦧温馨提示\",温馨提示\n")
+        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
 
-        f.write("#EXTINF:-1 tvg-id=\"谨防诈骗\" tvg-name=\"谨防诈骗\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/谨防诈骗.png\" group-title=\"🦧温馨提示\",谨防诈骗\n")
-        f.write("https://iCloud.5iclub.dpdns.org/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"谨防诈骗\" tvg-name=\"谨防诈骗\" tvg-logo=\"https://logo.sspai.pp.ua/img/谨防诈骗.png\" group-title=\"🦧温馨提示\",谨防诈骗\n")
+        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
 
-        f.write("#EXTINF:-1 tvg-id=\"禁止蕉绿\" tvg-name=\"禁止蕉绿\" tvg-logo=\"https://logo.5iclub.dpdns.org/tv/禁止蕉绿.png\" group-title=\"🦧温馨提示\",禁止蕉绿\n")
-        f.write("https://iCloud.5iclub.dpdns.org/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"禁止蕉绿\" tvg-name=\"禁止蕉绿\" tvg-logo=\"https://logo.sspai.pp.ua/img/禁止蕉绿.png\" group-title=\"🦧温馨提示\",禁止蕉绿\n")
+        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
         
         # 写入频道信息
         for channel_info in deduped_channels:
