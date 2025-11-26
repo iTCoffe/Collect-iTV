7531b7a9133befcf736de50b93d0f71c155d112c
IT咖啡館 <176115520+iTCoffe@users.noreply.github.com>
2025-11-26 21:09:00 +0800

    Update iptv.py

--- .github/workflows/iptv.py
+++ .github/workflows/iptv.py
@@ -321,14 +321,14 @@ def generate_output_files(valid_urls, cctv_channels, province_channels, m3u_file
         f.write("#EXTM3U x-tvg-url=\"https://itv.sspai.pp.ua/epg.xml\" catchup=\"append\" catchup-source=\"?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}\"\n")
 
         # 添加新的 EXTINF 行
-        f.write("#EXTINF:-1 tvg-id=\"温馨提示\" tvg-name=\"温馨提示\" tvg-logo=\"https://logo.sspai.pp.ua/img/温馨提示.png\" group-title=\"🦧温馨提示\",温馨提示\n")
-        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"温馨提示\" tvg-name=\"温馨提示\" tvg-logo=\"https://logo.jsdelivr.dpdns.org/tv/温馨提示.png\" group-title=\"🦧温馨提示\",温馨提示\n")
+        f.write("https://iCloud.jsdelivr.dpdns.org/video/Disclaimer.mp4\n")
 
-        f.write("#EXTINF:-1 tvg-id=\"谨防诈骗\" tvg-name=\"谨防诈骗\" tvg-logo=\"https://logo.sspai.pp.ua/img/谨防诈骗.png\" group-title=\"🦧温馨提示\",谨防诈骗\n")
-        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"谨防诈骗\" tvg-name=\"谨防诈骗\" tvg-logo=\"https://logo.jsdelivr.dpdns.org/tv/谨防诈骗.png\" group-title=\"🦧温馨提示\",谨防诈骗\n")
+        f.write("https://iCloud.sjsdelivr.dpdns.org/video/Disclaimer.mp4\n")
 
-        f.write("#EXTINF:-1 tvg-id=\"禁止蕉绿\" tvg-name=\"禁止蕉绿\" tvg-logo=\"https://logo.sspai.pp.ua/img/禁止蕉绿.png\" group-title=\"🦧温馨提示\",禁止蕉绿\n")
-        f.write("https://iCloud.sspai.pp.ua/video/Disclaimer.mp4\n")
+        f.write("#EXTINF:-1 tvg-id=\"禁止蕉绿\" tvg-name=\"禁止蕉绿\" tvg-logo=\"https://logo.jsdelivr.dpdns.org/tv/禁止蕉绿.png\" group-title=\"🦧温馨提示\",禁止蕉绿\n")
+        f.write("https://iCloud.jsdelivr.dpdns.org/video/Disclaimer.mp4\n")
         
         # 写入频道信息
         for channel_info in deduped_channels:
